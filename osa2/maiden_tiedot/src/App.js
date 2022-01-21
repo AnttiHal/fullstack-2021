708e920699c7b124581bb42e7cfdb0c82536fcdf
AnttiHalmetoja <anttihalmetoja@gmail.com>
2022-01-21 09:40:40 +0200

    2.13 maiden tiedot

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -3,6 +3,52 @@ import axios from 'axios'
 
 
 
+const SelectedCountry = ({country}) => {
+  return (
+    <>
+      <h1>{country.name}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map(lang => 
+        <li key={lang.name}>{lang.name}</li>)}
+      </ul>  
+      <img src={country.flag} alt='' width='300px'/>
+    </>
+  )
+}
+
+const FilterResults = ({countriesToShow, setCountriesToShow}) => {
+  if (countriesToShow.length>10){
+    return (
+      'Too many matches, specify more'
+    )
+  } else if (countriesToShow.length===1) {
+  return (
+     <SelectedCountry country={countriesToShow[0]}/>
+  )} else if (countriesToShow.length<10) {
+    return (
+      countriesToShow.map(country => {
+        return (
+          
+        <div key={country.name}>
+          {country.name}  &nbsp;
+          <button onClick={() => setCountriesToShow([country])}>              
+          show
+          </button>         
+        </div>
+      )})
+    )
+  } 
+  return (
+    'No results'
+  )
+}
+
+
+
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [countriesToShow, setCountriesToShow] = useState([])
@@ -19,42 +65,22 @@ const App = () => {
   })
   }, [])
   console.log('render', countries.length, 'countries')
+  console.log('countriesToShow: ', countriesToShow.length)
 
   const handleInputChange = (event) => {
     setText(event.target.value)
     console.log(event.target.value)
-    
     setCountriesToShow(countries.filter(country => (country.name.toLowerCase()).includes(event.target.value.toLowerCase())))
   }
 
-  const filteredCountries = countriesToShow.length>10 ? 'Too many matches, specify more': countriesToShow
+  
+
   return (
   <div>
-    <p>find countries
+    <h1>Welcome to country finder</h1>
+    find countries &nbsp;
     <input value={text} onChange={handleInputChange}></input>
-    </p>
-      
-      {countriesToShow.length>10 ? 'Too many matches, specify more': 
-      countriesToShow.length===1 ? countriesToShow.map(country => 
-      <li key={country.altSpellings[0]}>
-      <h1>{country.name}</h1>
-      <p>Capital {country.capital}</p>
-      <p>Population {country.population}</p>
-      <h2>Languages</h2>
-      <ul>
-        {country.languages.map(lang => 
-          <li>{lang.name}</li>)}
-      </ul>  
-      <img src={country.flag} alt='' width='300px'/>
-    </li>
-      ): countriesToShow.length<10 ? countriesToShow.map(country => 
-        <li key={country.altSpellings[0]}>
-          {country.name}
-          <button>
-            show
-          </button>
-        </li> ) : 'No results'}
-    
+    <FilterResults countriesToShow={countriesToShow} setCountriesToShow={setCountriesToShow}/>  
   </div>
 )
 }
