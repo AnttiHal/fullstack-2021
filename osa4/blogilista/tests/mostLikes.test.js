64bbe2896752a30e0a94adaa58e55f75a7c69de5
AnttiHalmetoja <anttihalmetoja@gmail.com>
2022-06-10 07:58:10 +0300

    4.6-4.7

--- osa4/blogilista/tests/mostLikes.test.js
+++ osa4/blogilista/tests/mostLikes.test.js
@@ -1,6 +1,6 @@
 const listHelper = require('../utils/list_helper')
 
-describe('most liked', () => {
+describe('most likes', () => {
     const listWithOneBlog = [
       {
         _id: '5a422aa71b54a676234d17f8',
@@ -63,33 +63,20 @@ describe('most liked', () => {
         }  
       ]
   
-    test('theres bigger list', () => {
-      const result = listHelper.favouriteBlog(listWithManyBlogs)
-      expect(result).toEqual({
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
+      test('there is only one blog in the list', () => {
+        const result = listHelper.mostLikes(listWithOneBlog)
+        expect(result).toEqual({
+            author: "Edsger W. Dijkstra",
+            likes: 5
+          })
       })
-    })
-
-    test('theres only one item in list', () => {
-      const result = listHelper.favouriteBlog(listWithOneBlog)
-      expect(result).toEqual({
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-      })
-    })
 
     
-
-      
-
-    
+      test('of the bigger list is the sum of likes', () => {
+        const result = listHelper.mostLikes(listWithManyBlogs)
+        expect(result).toEqual({
+            author: "Edsger W. Dijkstra",
+            likes: 17
+          })
+      })
   })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -13,8 +13,77 @@ const dummy = (blogs) => {
     const max = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
     return max;
   }
+
+  const mostBlogs = (blogs) => {
+    let uniqueAuthors = []
+    let mostBlogsIndex = 0;
+    
+    for (let i =0;i<blogs.length;i++) {
+        if (!(uniqueAuthors.includes(blogs[i].author))) {  
+          uniqueAuthors.push(blogs[i].author)
+          
+      }    
+    }
+
+    let uniqueAuthorsblogCount = new Array(uniqueAuthors.length).fill(0)
+    console.log('count '+ uniqueAuthorsblogCount)
+
+
+    for (let i =0;i<uniqueAuthors.length;i++) {
+        for (let j=0;j<blogs.length;j++) {
+          if (uniqueAuthors[i]===blogs[j].author) {
+            uniqueAuthorsblogCount[i] +=1
+          }
+          if (uniqueAuthorsblogCount[i]>uniqueAuthorsblogCount[mostBlogsIndex]) {
+            mostBlogsIndex = i
+          }
+        }
+           
+    }
+    
+    let obj = {
+        author: uniqueAuthors[mostBlogsIndex],
+        blogs: uniqueAuthorsblogCount[mostBlogsIndex]
+    }
+    return obj;
+  }
+
+  const mostLikes = (blogs) => {
+    let uniqueAuthors = []
+    let mostLikesIndex = 0;
+    
+    for (let i =0;i<blogs.length;i++) {
+        if (!(uniqueAuthors.includes(blogs[i].author))) {  
+          uniqueAuthors.push(blogs[i].author)
+          
+      }    
+    }
+
+    let uniqueAuthorsLikeCount = new Array(uniqueAuthors.length).fill(0)
+    console.log('count '+ uniqueAuthorsLikeCount)
+
+
+    for (let i =0;i<uniqueAuthors.length;i++) {
+        for (let j=0;j<blogs.length;j++) {
+          if (uniqueAuthors[i]===blogs[j].author) {
+            uniqueAuthorsLikeCount[i] += blogs[j].likes
+          }
+          if (uniqueAuthorsLikeCount[i]>uniqueAuthorsLikeCount[mostLikesIndex]) {
+            mostLikesIndex = i
+          }
+        }
+           
+    }
+    
+    let obj = {
+        author: uniqueAuthors[mostLikesIndex],
+        likes: uniqueAuthorsLikeCount[mostLikesIndex]
+    }
+    return obj;
+  }
+
   
   module.exports = {
     dummy,
-    totalLikes, favouriteBlog
+    totalLikes, favouriteBlog, mostBlogs, mostLikes
   }
