530afaede3602f2e7902f25c059924efe3dd9476
AnttiHalmetoja <anttihalmetoja@gmail.com>
2022-06-24 06:26:04 +0300

    7.8

--- osa7/ultimate-hooks/src/App.js
+++ osa7/ultimate-hooks/src/App.js
@@ -17,11 +17,18 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
-  
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }, [setResources, baseUrl])
+
   // ...
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
